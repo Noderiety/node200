6e86f4a8f5b3b42389399a68ebec5618c31f99bf
Adam Crabtree <dude@noderiety.com>
2014-10-29 12:08:55 -0700

    Add express 4 & streaming router example

--- src/express_streamingrouter.es6.js
+++ src/express_streamingrouter.es6.js
@@ -4,6 +4,7 @@ let express = require('express')
 	, host = '127.0.0.1'
 	// Create an express server
 	, app = express()
+	, streamingRouter = express.Router()
 
 // Http request logging
 app.use(require('morgan')('combined'))
@@ -14,14 +15,18 @@ app.use(require('serve-favicon')(__dirname + '/../static/favicon.ico'))
 // Static file support
 app.use(require('serve-static')(__dirname + '/../static', {'index': false}))
 
+streamingRouter.all('/echo/', (req, res) => {
+	console.log('echo')
+	req.pipe(res)
+})
+
+app.use(streamingRouter)
+
 // Add req.body
 app.use(require('body-parser').urlencoded())
 app.use(require('body-parser').json())
 app.use(require('body-parser').text())
 
-app.all('/echo/', (req, res) => {
-	req.pipe(res)
-})
 app.get('/', (req, res) => {
 	res.send('Hello world')
 })
